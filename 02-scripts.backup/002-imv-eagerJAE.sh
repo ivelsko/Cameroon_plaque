8f244f6ca302e8f5f6d1d24a20c4bbe4f0e95dee
Irina Velsko <velsko@mpi-sdag1.sdag.ppj.shh.mpg.de>
2019-10-18 13:00:46 +0200

    increased memory

--- 02-scripts.backup/002-imv-eagerJAE.sh
+++ 02-scripts.backup/002-imv-eagerJAE.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
  
 #SBATCH -c 4
-#SBATCH --mem=32000
+#SBATCH --mem=48000
 #SBATCH --partition=medium
 #SBATCH -o /projects1/clusterhomes/velsko/slurm_logs/slurm.%j.out
 #SBATCH -e /projects1/clusterhomes/velsko/slurm_logs/slurm.%j.err
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=time_limit
 #SBATCH --mail-use=velsko@shh.mpg.de
-#SBATCH --array=0-39%4
+#SBATCH --array=0-9%4
 #SBATCH -J "cmc_eager"
  
 SAMPLES=( $(find /projects1/microbiome_calculus/Cameroon_plaque/03-preprocessing/human_filering/output_JAE/ -name '*.xml' -type f) )
