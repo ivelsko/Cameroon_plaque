6d4b4c396686f5bf3b1db5ddb8deb739dbd4ee79
Irina Marie Velsko <velsko@itmp132753.w-staff.shh.mpg.de>
2019-10-23 10:15:11 +0200

    more analyses

--- 02-scripts.backup/008-imv-maltRefSeqCustomJAE.sh
+++ 02-scripts.backup/008-imv-maltRefSeqCustomJAE.sh
@@ -10,6 +10,6 @@ sbatch \
 --mail-user=velsko@shh.mpg.de \
 -J "MaltHMPref" \
 --wrap="/projects1/microbiome_calculus/evolution/02-scripts.backup/008-malt-genbank-refseq_bacarchhomo_gcs_20181122_4step_85pc_supp_0.01 \
-/projects1/microbiome_calculus/cameroon_hunter_gatherer_calculus/04-analysis/malt/input-temp_JAE/*.gz \
-/projects1/microbiome_calculus/cameroon_hunter_gatherer_calculus/04-analysis/malt/RefSeqCustom"
+/projects1/microbiome_calculus/Cameroon_plaque/04-analysis/malt/input-temp_JAE/*.gz \
+/projects1/microbiome_calculus/Cameroon_plaque/04-analysis/malt/RefSeqCustom"
 
