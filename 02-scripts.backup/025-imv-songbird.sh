1798e1ac4a8a848473fc2daff63bded8373cb905
Irina Velsko <velsko@mpi-sdag1>
2020-07-22 16:35:23 +0200

    add files for songbird differential abundance testing

---
+++ 02-scripts.backup/025-imv-songbird.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sbatch \
+-c 4 \
+--mem=32000 \
+--partition=short \
+-o ~/slurm_logs/slurm.%j.out \
+-e ~/slurm_logs/slurm.%j.err \
+--mail-type=fail \
+--mail-type=time_limit \
+--mail-user=velsko@shh.mpg.de \
+--wrap="songbird multinomial \
+	--input-biom /projects1/microbiome_calculus/Cameroon_plaque/05-results.backup/malt_genus_decontam.biom \
+	--metadata-file /projects1/microbiome_calculus/Cameroon_plaque/00-documentation.backup/01-cameroon_hunter_gatherer_metadata.tsv \
+	--formula "1" \
+	--epochs 10000 \
+	--differential-prior 0.5 \
+	--summary-interval 1 \
+	--summary-dir /projects1/microbiome_calculus/Cameroon_plaque/04-analysis/songbird/run1"
