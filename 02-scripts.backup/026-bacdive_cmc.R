554a112f96844e6268c79e790d79bda7ede6cbd5
Irina Marie Velsko <ivelsko@users.noreply.github.com>
2020-09-26 10:52:51 +0200

    added tax & seed analysis modifications

--- 02-scripts.backup/026-bacdive_cmc.R
+++ 02-scripts.backup/026-bacdive_cmc.R
@@ -4,9 +4,23 @@ library(BacDiveR)
 
 ## DATA
 ## Load heatmap block data
-#input_data <- "/home/fellows/projects1/microbiome_calculus/evolution/04-analysis/screening/compositional_heatmaps.backup/block_summaries/Heatmap_ManualBlockDescriptions_alltaxa_minsupportmultiplier4_minprevalence4_databasent_20190807.tsv"
-input_data <- "/home/fellows/projects1/microbiome_calculus/evolution/04-analysis/screening/compositional_heatmaps.backup/block_summaries/Heatmap_ManualBlockDescriptions_alltaxa_minsupportmultiplier4_minprevalence4_databaserefseq_20190807.tsv"
-table_nt <- read_tsv(input_data)
+species_ancom <- fread("/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/ancomBC_species_noblanks_HMP_HG.tsv", sep = "\t") 
+species_list <- species_ancom %>%
+  select(Species) %>% 
+  bind_rows(., fread("/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/ancomBC_species_noblanks_JAE_HG.tsv", sep = "\t") %>%
+              select(Species)) %>%
+  bind_rows(., fread("/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/ancomBC_species_cmc_FA_FC.tsv", sep = "\t") %>%
+              select(Species)) %>%
+  bind_rows(., fread("/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/ancomBC_species_cmc_FA_LR.tsv", sep = "\t") %>%
+              select(Species)) %>%
+  bind_rows(., fread("/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/ancomBC_species_cmc_sex.tsv", sep = "\t") %>%
+              select(Species)) %>%
+  bind_rows(., fread("/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/ancomBC_species_cmc_toothsite.tsv", sep = "\t") %>%
+              select(Species)) %>%
+  distinct() %>%
+  rename(Taxon = Species)
+
+table_nt <- species_list
 
 ## set some additional prior info
 bacdive_tibble_names <- c("bacdive_id", "section", "subsection", "field")
@@ -66,16 +80,6 @@ table_nt_meta <- table_nt %>%
   group_split(Length)
   
   
-table_nt_meta_rep <- table_nt_meta[[2]] %>%   
-  filter(Length != 1) %>%
-  unnest() %>%
-  select(-bacdive_id) %>%
-  distinct %>%
-  filter(!section %in% c("taxonomy_name", "application_interaction", "strain_availability", "references"),
-         !grepl("_reference|text_mined", field),
-         !grepl("sequence", subsection)) %>%
-  print()
-
 table_nt_meta_new <- table_nt_meta[[1]] %>%
   mutate(BacDive_Result = map(Taxon, bacdive_to_tibble)) %>%
   mutate(Length = map(BacDive_Result, length) %>% unlist) %>%
@@ -88,8 +92,20 @@ table_nt_meta_new <- table_nt_meta[[1]] %>%
          !grepl("sequence", subsection)) %>%
   print()
 
-table_nt_meta_final <- bind_rows(table_nt_meta_rep, table_nt_meta_new)
-  
+table_nt_meta_rep <- table_nt_meta[[1]] %>%
+  filter(Length != 1) %>%
+  unnest(c(BacDive_Result)) %>%
+  # select(-bacdive_id) %>%
+  distinct %>%
+  filter(!section %in% c("taxonomy_name", "application_interaction", "strain_availability", "references"),
+         !grepl("_reference|text_mined", field),
+         !grepl("sequence", subsection)) %>%
+  print()
+
+
+# table_nt_meta_final <- bind_rows(table_nt_meta_rep, table_nt_meta_new)
+table_nt_meta_final <-table_nt_meta_new
+
 
 ## Get oxygen data only
 data_oxygen <- table_nt_meta_final %>% 
@@ -104,16 +120,7 @@ data_gram <- table_nt_meta_final %>%
   group_by(Taxon) %>%
   summarise(aggregate_gram_stain = paste0(unique(value), collapse = "/"))
 
-data_isosource <- table_nt_meta_final %>% 
-  select(Taxon, field, value) %>%
-  filter(grepl("sample_type", field)) %>%
-  group_by(Taxon) %>%
-  summarise(aggregate_sample_type = paste0(unique(value), collapse = "/")) %>%
-  mutate(oral_related = grepl("oral|dental|caries|plaque|perio|mouth|dentine|gingiva|saliva", aggregate_sample_type),
-         oral_related = map(oral_related, function(x) if_else(isFALSE(x), NA, x)) %>% unlist) %>%
-  select(-aggregate_sample_type)
+data_meta_final <- left_join(data_oxygen, data_gram)
 
-data_meta_final <- left_join(data_oxygen, data_gram) %>%
-  left_join(data_isosource)
-
-write_tsv(data_meta_final, paste0(tools::file_path_sans_ext(input_data), "_metadata.tsv"))
+fwrite(data_meta_final, "/Users/velsko/Dropbox (Personal)/MPI-SHH/CMC/Cameroon_plaque/05-results.backup/bacdiveR_metadata.tsv", sep = "\t", quote = F)
+# write_tsv(data_meta_final, paste0(tools::file_path_sans_ext(species_ancom), "_metadata.tsv"))
